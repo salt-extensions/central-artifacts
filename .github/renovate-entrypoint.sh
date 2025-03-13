2f74ac2b2055ee6ffaf1c031ac9021a28db2b274
jeanluc <lkubb@protonmail.com>
2025-03-13 16:56:47 +0100

    Debugging entrypoint

--- .github/renovate-entrypoint.sh
+++ .github/renovate-entrypoint.sh
@@ -2,9 +2,13 @@
 
 args=
 
+echo "In entrypoint"
+
 if [ -n "$IGNORE_SCHEDULE" ]; then
     echo "Ignoring schedule"
     args="--schedule="
+else
+    echo "Using schedule"
 fi
 
 exec renovate $args
